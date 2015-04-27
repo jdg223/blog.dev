5ff5b50f12a158459bb2dc5b0ca226afeb9fff2b
J.D. Garza <jdgarza22@yahoo.com>
2015-04-27 14:42:35 -0500

    files for a dice game and a file that will echo name

---
+++ app/views/my-first-view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<H1>Hello, <?php echo $name; ?>!</H1>
+<p><?php echo $another; ?></p>
+</body>
+</html>
---
+++ app/views/roll-dice.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h2>You Rolled: <?php echo $guess; ?></h2>
+	<h2>The Random Dice is: <?php echo $diceNumber; ?></h2>
+		<?php if($guess == $diceNumber):?>
+			<h1>You Win</h1>
+		<?php endif; ?>
+</body>
+</html>
