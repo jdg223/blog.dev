6d204b3d612a6b16e3a873d0fb6382c3cd3a7c54
J.D. Garza <jdgarza22@yahoo.com>
2015-04-29 13:28:59 -0500

    added home controller function to root

--- app/routes.php
+++ app/routes.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/','HomeController@showWelcome');
 
 Route::get('sayhello/{name}','HomeController@sayHello');
 
