b52a202301667f4ccbb781b06c2f138a2d07df36
J.D. Garza <jdgarza22@yahoo.com>
2015-05-01 15:30:30 -0500

    updated routes

--- app/routes.php
+++ app/routes.php
@@ -22,3 +22,21 @@ Route::get('portfolio','HomeController@showPortfolio');
 Route::get('rolldice/{dice}','HomeController@rollDice');
 
 Route::resource('posts', 'PostsController');
+
+Route::get('orm-test',function()
+{
+	$post = new Post();
+	$post->title = 'New blog';
+	$post->body = 'Lorem Ipsum Dolor';
+	$post->save();
+
+	$post2 = new Post();
+	$post2->title = 'New Entry';
+	$post2->body = 'Lorem Ipsum Dolor';
+	$post2->save();
+
+	$post3 = new Post();
+	$post3->title = 'New Entry 3';
+	$post3->body = 'Lorem Ipsum Dolor';
+	$post3->save();
+});
