4526cbdbef72919d1e92158b6676360f00094a84
J.D. Garza <jdgarza22@yahoo.com>
2015-05-29 15:30:59 -0500

    moved analytics to head tag

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -16,6 +16,17 @@
 	
 	<!-- Google Font -->
 	<link href='http://fonts.googleapis.com/css?family=Raleway:400,200%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+
+	<!-- Google Analytics -->
+	<script type="text/javascript">
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-63551459-1', 'auto');
+	  ga('send', 'pageview');
+	</script>	
 	
 </head>
 <body>
@@ -386,17 +397,6 @@
 	<script src="js/jquery.valid.js"></script>
 	<script src="js/portfolio-plugins.js"></script>
 	<script src="js/portfolio.js"></script>
-	
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-63551459-1', 'auto');
-	  ga('send', 'pageview');
-	</script>
 		
 </body>
 </html>
